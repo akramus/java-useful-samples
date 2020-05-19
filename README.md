19de1a3fde87fa134321ae3a714991666abd800c
Akram DHAOUI <akram-externe.dhaoui@enedis.fr>
2020-05-19 19:54:09 +0200

    add file management sample

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ jdk-api
 -------
 - format date to String 
 - parse string to date
-
+- create tmp file and all lines as string
+- read a file and display all lines
 csv
 ----
 - coming soon !
--- jdk-api/src/test/java/fr/adiveo/samples/FilesHandling.java
+++ jdk-api/src/test/java/fr/adiveo/samples/FilesHandling.java
@@ -1,7 +1,50 @@
 package fr.adiveo.samples;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * : LCR - 19/05/2020.
  */
 public class FilesHandling {
+
+
+    @Test
+    public void test_create_temp_files() throws IOException {
+
+        Path tmpFile = Files.createTempFile("export", "users_");
+        BufferedWriter writer = Files.newBufferedWriter(tmpFile, Charset.defaultCharset());
+        writer.write("This is a test");
+        writer.flush();
+        writer.close();
+
+        assertEquals("This is a test",Files.readString(tmpFile));
+
+    }
+
+    @Test
+    public void test_read_file() throws IOException {
+
+        Path file1  = Paths.get("src/test/resources/test.txt");
+        List<String> allLines  = Files.readAllLines(file1);
+        assertEquals(4,allLines.size());
+        assertEquals("This is a test",allLines.get(0));
+        assertEquals("Second line",allLines.get(1));
+        assertEquals("Third line",allLines.get(2));
+        assertEquals("End",allLines.get(3));
+
+    }
+
+
+
+
 }
